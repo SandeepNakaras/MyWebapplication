eb0ed71d6bb5d29e593b9b38e747521737b589c7
sandeep nakarakommula <sandeep nakarakommula>
2016-04-25 16:53:22 -0500

    Restservice

--- src/main/java/service/state/tx/controller/RestServiceImpl.java
+++ src/main/java/service/state/tx/controller/RestServiceImpl.java
@@ -1,6 +1,7 @@
 package service.state.tx.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import service.state.tx.model.Coffee;
 
@@ -11,8 +12,11 @@ public class RestServiceImpl {
 	public void getListofMenu(Coffee coffee){
 		coffee.getId();
 		coffee.getCoffeeType();
-		
-		
 	}
-
+	
+	
+	@RequestMapping(value="/testConnection", method = RequestMethod.GET)
+	public String testConnection(){
+		return "Success";
+	}
 }
